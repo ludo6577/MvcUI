b56859182c9df29e379848a7268d870e784bb683
ludo6577 <ludo6577@gmail.com>
2015-01-06 22:51:26 +0100

    Listbox with Checkbox

---
+++ MvcUI/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcUI
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jquery.effects.core.js",
+                        "~/Scripts/jquery.effects.blind.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/Site.css"));
+        }
+    }
+}
